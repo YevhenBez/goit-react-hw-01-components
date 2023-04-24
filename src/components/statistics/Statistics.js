4321d6ad7b2af8e4b365eeecee8a8928cbc856ca
YevhenBez <byxgalter@bigmir.net>
2023-04-24 10:24:44 +0300

    Убрал не нужный кусок кода Задание 2 Модуль 1 REACT
    Убрал не нужный кусок кода Задание 2 Модуль 1 REACT

--- src/components/statistics/Statistics.js
+++ src/components/statistics/Statistics.js
@@ -11,18 +11,6 @@ const Statistics = (props) => {
                         <div><span class="percentage">{elementOfStats.percentage}%</span></div>
                     </div>
                 </li>))}
-                {/* <li class="item">
-                    <span class="label">.mp3</span>
-                    <span class="percentage">14%</span>
-                </li>
-                <li class="item">
-                    <span class="label">.pdf</span>
-                    <span class="percentage">41%</span>
-                </li>
-                <li class="item">
-                    <span class="label">.mp4</span>
-                    <span class="percentage">12%</span>
-                </li> */}
             </ul>
         </section>
     )
