2d312e3d93c061e33ed643740c773ea1463a1fd2
YevhenBez <byxgalter@bigmir.net>
2023-04-26 23:56:13 +0300

    Импортнул библиотеку    prop-types   и прописал типы Пропсов при помощи неё. Доделки Задания 2 Модуля 1   REACT
    Импортнул библиотеку    prop-types   при помощи    import PropTypes from 'prop-types';  и прописал типы Пропсов при помощи неё. При этом обрати внимание, что в данном случае второй Пропс    stats    - это массив объектов, поэтому первая команда библиотеки   prop-types
       , а именно  PropTypes.arrayOf     проверяет массив чего пришёл. А вторая команда библиотеки   prop-types     , а именно    PropTypes.shape({   })     проверяет уже внутренности объекта. Доделки Задания 2 Модуля 1   REACT

--- src/components/statistics/Statistics.js
+++ src/components/statistics/Statistics.js
@@ -1,3 +1,5 @@
+import PropTypes from 'prop-types';
+
 const Statistics = (props) => {
     const { title, stats } = props;
     return (
@@ -14,6 +16,15 @@ const Statistics = (props) => {
             </ul>
         </section>
     )
+}
+
+Statistics.propTypes = {
+    title: PropTypes.string,
+    stats: PropTypes.arrayOf(PropTypes.shape({
+        id: PropTypes.string.isRequired,
+        label: PropTypes.string.isRequired,
+        percentage: PropTypes.number.isRequired,
+    }))
 }
 
 export default Statistics;
