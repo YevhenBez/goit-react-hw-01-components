623d40f7144d76a208b7ca94e5a07076714daa32
YevhenBez <byxgalter@bigmir.net>
2023-04-28 13:43:12 +0300

    Переделал на Модульный-CSS   Доделки Задание 3 Модуль 1 REACT
    Переделал на Модульный-CSS   Для этого в файле  index.js    убрал импорт Ванильного CSS    Переименовал файл    friendList.css    на   friendList.module.css       В файле FriendListItem.js   сделал импорт Модульного-CSS    при помощи   import css from './css/friendList.module.css';    и
    передал Модульные стили при помощи  {css.friends__item}     и      {css.status}    и т.д. и т.п. Доделки Задание 3 Модуль 1 REACT

--- src/components/friendList/FriendListItem.js
+++ src/components/friendList/FriendListItem.js
@@ -1,13 +1,14 @@
 import PropTypes from 'prop-types';
+import css from './css/friendList.module.css';
 import { AiTwotoneCheckCircle } from "react-icons/ai";
 import { AiTwotoneCloseCircle } from "react-icons/ai";
 
 const FriendListItem = (props) => {
     const { avatar, name, isOnline } = props;
     return (
-        <li className="friends__item" >
-            {isOnline ? <span className="status" ><AiTwotoneCheckCircle size={36} color="green"/></span> : <span className="status"><AiTwotoneCloseCircle size={36} color="red"/></span>}
-            <img className="avatar" src={avatar} alt="User avatar" width="98" />
+        <li className={css.friends__item} >
+            {isOnline ? <span className={css.status} ><AiTwotoneCheckCircle size={36} color="green" /></span> : <span className={css.status}><AiTwotoneCloseCircle size={36} color="red"/></span>}
+            <img className={css.avatar} src={avatar} alt="User avatar" width="98" />
             <p className="name">{name}</p>
         </li>
     );
