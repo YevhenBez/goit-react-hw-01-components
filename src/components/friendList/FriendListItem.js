3eb1f30764a83ad71354b952d33edc9c6e99e257
YevhenBez <byxgalter@bigmir.net>
2023-04-29 17:02:33 +0300

    Переделал изменение цвета фишки в зависимости от значения isOnline    на изменение цвета в стилях. Корректировка Задания 3 Модуля 1 REACT
    Переделал изменение цвета фишки в зависимости от значения isOnline    на изменение цвета в стилях. Для этого в файле  friendList.module.css    сделал стили    .statusRed      и      .statusGreen    с соответствующими цветами. А в файле    FriendListItem.js       в   span    через Тернарник в зависимости от значения   isOnline   передал соответствующие стили. Также, в связи с выше изложенным убрал из этого файла не нужный импорт одной из иконок и не нужную логику. Корректировка Задания 3 Модуля 1 REACT

--- src/components/friendList/FriendListItem.js
+++ src/components/friendList/FriendListItem.js
@@ -1,13 +1,12 @@
 import PropTypes from 'prop-types';
 import css from './css/friendList.module.css';
 import { AiTwotoneCheckCircle } from "react-icons/ai";
-import { AiTwotoneCloseCircle } from "react-icons/ai";
 
 const FriendListItem = (props) => {
     const { avatar, name, isOnline } = props;
     return (
         <li className={css.friends__item} >
-            {isOnline ? <span className={css.status} ><AiTwotoneCheckCircle size={36} color="green" /></span> : <span className={css.status}><AiTwotoneCloseCircle size={36} color="red"/></span>}
+            <span className = {isOnline ? css.statusGreen : css.statusRed } ><AiTwotoneCheckCircle size={36} /></span>
             <img className={css.avatar} src={avatar} alt="User avatar" width="98" />
             <p className="name">{name}</p>
         </li>
